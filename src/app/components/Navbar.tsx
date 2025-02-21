18b774694895f08eedb7d4deb01ed693f534f069
Ryley <ryleyenasanden@gmail.com>
2025-02-20 20:20:56 -0700

    Initial implimentation of site with a navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,38 @@
+"use client"
+import Link from "../../../node_modules/next/link";
+import {useState} from "react";
+
+export const NavBar = () => {
+//const [isOpen, setIsOpen] = useState(false)
+    return(
+        <nav className="bg-gray-800 text-white p-4 sm:p-6 md:flex md:justify-between
+        md:items-center">
+            <div className="container mx-auto flex justify-between items-center">
+                <a href ="" className="text-2xl font-bold">
+                    Group 5
+                </a>
+                <div className="hidden md:flex">
+                    <Link href ="/" className="mx-2 hover:text-gray-300">
+                        Home
+                    </Link>
+                    <Link href ="/staff" className="mx-2 hover:text-gray-300">
+                        Staff
+                    </Link>
+                    <Link href ="/resources" className="mx-2 hover:text-gray-300">
+                        Resources
+                    </Link>
+                
+                </div>
+                
+                <div className="md:hidden flex items-center">
+                    <button onClick={()=>{
+                        //setIsOpen(!isOpen);
+                    }}></button> 
+                
+                </div>
+            
+                
+            </div>
+        </nav>
+    )
+}
