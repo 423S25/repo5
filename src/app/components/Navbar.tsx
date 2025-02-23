17fbcf0c73784a46bb1a1a31df91e4fbd882f64a
Mark Kwapisz <slashelephant@gmail.com>
2025-02-23 11:23:26 -0700

    added search bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,12 @@
 "use client"
-import Link from "../../../node_modules/next/link";
-import {useState} from "react";
+import Link from "next/link";
+import { useState } from "react";
 
 export const NavBar = () => {
-//const [isOpen, setIsOpen] = useState(false)
+    const [searchText, setSearchText] = useState("");
+
     return(
-        <nav className="bg-gray-800 text-white p-4 sm:p-6 md:flex md:justify-between
-        md:items-center">
+        <nav className="bg-gray-800 text-white p-4 sm:p-6 md:flex md:justify-between md:items-center">
             <div className="container mx-auto flex justify-between items-center">
                 <a href ="" className="text-2xl font-bold">
                     Group 5
@@ -21,18 +21,26 @@ export const NavBar = () => {
                     <Link href ="/resources" className="mx-2 hover:text-gray-300">
                         Resources
                     </Link>
+                </div>
                 
+                <div className="hidden md:flex items-center ml-4">
+                    <input 
+                        type="text" 
+                        placeholder="Search..." 
+                        className="px-2 py-1 text-black rounded" 
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                            console.log(e.target.value);
+                        }}
+                    />
                 </div>
                 
                 <div className="md:hidden flex items-center">
-                    <button onClick={()=>{
+                    <button onClick={() => {
                         //setIsOpen(!isOpen);
                     }}></button> 
-                
                 </div>
-            
-                
             </div>
         </nav>
-    )
+    );
 }
