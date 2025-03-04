de9a5e5be7fff3ca9c09f7a53ce653f797124af3
Ryley <ryleyenasanden@gmail.com>
2025-03-04 11:01:49 -0700

    home page test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function HomePage() {
     <div>
       <NavBar />
       <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+      <p>This is a test to see if branch worked.</p>
     </div>
   );
 }
