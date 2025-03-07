33675e721f05abcfb063c3c982e75a574024feb5
Markwapisz <Slashelephant@gmail.com>
2025-03-07 15:13:44 -0700

    Fixed so Navbar is now outside, full width on Help page

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -22,9 +22,13 @@ export default function HelpPage() {
     faq.question.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  return (
-    <div className="container mx-auto p-6">
-      <NavBar />
+  //return (
+    //<div className="container mx-auto p-6">
+      //<NavBar />
+    return (
+    <>
+      <NavBar /> 
+      <div className="container mx-auto p-6">
 
       <div className="bg-white shadow-md p-6 rounded-lg">
         <h1 className="text-3xl font-bold mb-4 text-center text-gray-800">Help & Support</h1>
