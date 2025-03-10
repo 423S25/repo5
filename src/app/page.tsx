12a4e41e54aa884f1aaa421a9091329a7eecece4
Markwapisz <Slashelephant@gmail.com>
2025-03-09 20:11:08 -0600

    Update page.tsx home page basics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,16 @@ export default function HomePage() {
   return (
     <div>
       <NavBar />
-      <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
-      <p>This test was to practice making branches and merging branches in a way 
-        that makes sense development wise.
-      </p>
+      <div className="text-center mt-10">
+        <h1 className="text-3xl font-bold">HRDC Intranet</h1>
+        <p className="text-gray-600 mt-2">
+          Welcome to the HRDC Intranet. You can use the navigation above to find important links, resources, and help.
+        </p>
+        <div className="mt-6 bg-gray-100 p-4 rounded">
+         <h2 className="text-xl font-semibold"> Announcements</h2>
+          <p className="text-gray-600">No new announcements.</p>
+        </div>
+      </div>
     </div>
   );
 }
