d5509c5023faa22a9f90aff822de54d1a9447b5b
Mark Kwapisz <slashelephant@gmail.com>
2025-03-25 20:14:18 -0600

    Fix duplicated ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,4 @@ module.exports = {
       "@next/next/no-img-element": "off"
     }
   };
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-    extends: "next/core-web-vitals",
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@next/next/no-img-element": "off"
-    }
-  };
-    
+  
