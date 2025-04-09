05715a77e68bc1490c1fbce11c9c52c13a3f746e
Sierrah Paul <sierrahpaul@gmail.com>
2025-04-09 10:29:34 -0600

    add search function to links page

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+export const dynamic = "force-dynamic";
+
 import { NavBar } from "../components/Navbar";
+import { getAnnouncements } from "@/utils/getAnnouncements";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function LinksPage() {
+  const [searchText, setSearchText] = useState("");
+  const router = useRouter();
+
   return (
     <>
       <NavBar /> {/* Navbar stays full width */}
@@ -24,11 +33,59 @@ export default function LinksPage() {
           paddingBottom: "100px",
           }}
         >
-     
+        
       <div className="container mx-auto p-6"> 
         <h1 className="text-3xl font-bold mb-4">Important Links</h1>
         <p>Welcome to the Important Links Page. Here you&apos;ll find links to websites you need to access often.</p>
        
+                 {/* Search Prompt Section */}
+      <div className="flex justify-center mt-10">
+        <div
+            style={{
+              width: 800,
+              background: "#147278",
+              borderRadius: 20,
+              boxShadow: "0px 3px 4px 6px rgba(0, 0, 0, 0.2)",
+              padding: 20,
+              color: "white",
+            }}
+          >
+        <h2 className="text-2xl font-bold mb-4">What can we help you find today?</h2>
+              <div
+                style={{
+                  background: "white",
+                  color: "black",
+                  borderRadius: 20,
+                  padding: 20,
+                }}
+              >
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    if (searchText.trim() !== "") {
+                      router.push(`/search?query=${searchText}`);
+                    }
+                  }}
+                  className="flex items-center"
+                >
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    className="border border-gray-300 rounded px-4 py-2 w-full text-black"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                  />
+                  <button
+                    type="submit"
+                    className="ml-2 bg-[#147278] text-white px-4 py-2 rounded"
+                  >
+                    Search
+                  </button>
+                </form>
+      </div>
+      </div>
+      </div>
+
         <h2 className="text-2xl font-semibold mt-6">Important Links</h2>
         <ul className="list-disc ml-6">
             <li className="mt-2">
