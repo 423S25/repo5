f3db8de20b57136cc509d5313c35d868150661ab
Sierrah Paul <sierrahpaul@gmail.com>
2025-04-09 10:22:00 -0600

    make HRDC Intranet more readable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function HomePageWrapper() {
 
       {/* Welcome Section */}
       <div className="text-center mt-10">
-        <h1 className="text-3xl font-bold mb-4">HRDC Intranet</h1>
+        <h2 className="text-3xl font-bold mb-4">HRDC Intranet</h2>
         <p className="text-gray-600 mt-2">
               Welcome to the HRDC Intranet. You can use the navigation above to find important links, resources, and help.
         </p>
