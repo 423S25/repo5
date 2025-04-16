5dc2ae86db5fb3d787e29e967484cb4b8aec0151
Sierrah Paul <sierrahpaul@gmail.com>
2025-04-16 10:12:23 -0600

    add font to links page

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -8,6 +8,20 @@ import { getAnnouncements } from "@/utils/getAnnouncements"; // I think not used
 import { useRouter } from "next/navigation"; // helps with redirecting/searching
 import { useState, useEffect } from "react"; // React hooks
 
+import { Montserrat } from 'next/font/google';
+
+const monsterrat = Montserrat({
+  weight: '600',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const monsterratBold = Montserrat({
+  weight: '900',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 // main function that shows the Links page
 export default function LinksPage() {
   const [searchText, setSearchText] = useState(""); // state for what user types in search box
@@ -50,8 +64,8 @@ export default function LinksPage() {
                 color: "white",
               }}
             >
-              <h1 className="text-3xl font-bold mb-4">Important Links</h1>
-              <p>Welcome to the Important Links Page. Here you'll find links to websites you need to access often.</p>
+              <h1 className={`${monsterratBold.className}`}>Important Links</h1>
+              <p className={`${monsterrat.className}`}>Welcome to the Important Links Page. Here you'll find links to websites you need to access often.</p>
             </div>
           </div>
        
@@ -67,7 +81,7 @@ export default function LinksPage() {
                 color: "white",
               }}
             >
-              <h2 className="text-2xl font-bold mb-4">What can we help you find today?</h2>
+              <h2 className={`${monsterratBold.className}`}>What can we help you find today?</h2>
               <div
                 style={{
                   background: "white",
@@ -116,7 +130,7 @@ export default function LinksPage() {
                 color: "white",
               }}
             >
-              <h2 className="text-2xl font-bold mb-4">Important Links</h2>
+              <h2 className={`${monsterratBold.className}`}>Important Links</h2>
               <div
                 style={{
                   background: "white",
@@ -129,15 +143,15 @@ export default function LinksPage() {
               >
                 <ul className="list-disc ml-6">
                   <li className="mt-2">
-                    <a target="_blank" href="https://www.paychex.com/login">Timesheet Login</a>
+                    <a className= {`${monsterrat.className}`} target="_blank" href="https://www.paychex.com/login">Timesheet Login</a>
                   </li>
                   <li>
-                    <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfN0Ju3jDKo3sLZPYz9IE93lLBKIG_SGxC7qsbcyCnT9_fR2w/viewform?usp=sf_link">
+                    <a  className= {`${monsterrat.className}`} target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfN0Ju3jDKo3sLZPYz9IE93lLBKIG_SGxC7qsbcyCnT9_fR2w/viewform?usp=sf_link">
                       New Hire Information Request
                     </a>
                   </li>
                   <li>
-                    <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfeVazdx3wStnG6HA6lqEG3qqR8Y4gf5YA6e5hI--JWG2yrNg/viewform?usp=sf_link">
+                    <a className= {`${monsterrat.className}`} target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfeVazdx3wStnG6HA6lqEG3qqR8Y4gf5YA6e5hI--JWG2yrNg/viewform?usp=sf_link">
                       Central Files Cover Sheet Submission Form
                     </a>
                   </li>
@@ -158,7 +172,7 @@ export default function LinksPage() {
                 color: "white",
               }}
             >
-              <h2 className="text-2xl font-bold mb-4">Need More Help?</h2>
+              <h2 className={`${monsterratBold.className}`}>Need More Help?</h2>
               <div
                 style={{
                   background: "white",
@@ -169,8 +183,8 @@ export default function LinksPage() {
                   overflowY: "auto",
                 }}
               >
-                <p>Ask your supervisor for help accessing a link you need.</p>
-                <p>
+                <p className = {`${monsterrat.className}`}>Ask your supervisor for help accessing a link you need.</p>
+                <p className = {`${monsterrat.className}`}>
                   Contact the HR department at{" "}
                   <a href="mailto:support@hrdc.org" className="text-blue-500">
                     support@hrdc.org
